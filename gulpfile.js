ceec88633bf121ea0df8233e97dac17d52454156
Csaba Valyi <Csaba_Valyi@epam.com>
2014-10-27 09:51:17 +0100

    modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var eventStream = require('event-stream');
 
 var path = {
     typescripts: './app/scripts/**/*.ts',
-    dist: './dist'
+    dist: './dist',
+    def: './app/raytrace.d.ts'
 };
 
 var tsProject = ts.createProject({
@@ -20,9 +21,11 @@ gulp.task('typescript', function () {
     var tsResult = gulp.src(path.typescripts)
         .pipe(ts(tsProject));
 
+    var definition = gulp.src(path.def);
     return eventStream.merge(
         tsResult.dts.pipe(gulp.dest(path.dist+'/definitions')),
-        tsResult.js.pipe(gulp.dest(path.dist+'/js'))
+        tsResult.js.pipe(gulp.dest(path.dist+'/js')),
+        definition.pipe(gulp.dest(path.dist))
     );
 });
 
